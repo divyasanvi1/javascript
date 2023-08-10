ae3539ac0e0c2c2a92ec9781c3ba3314771a2150
divyasanvi1 <108477155+divyasanvi1@users.noreply.github.com>
2023-08-10 22:27:24 +0530

    Add files via upload

---
+++ fibonnaci.js
@@ -0,0 +1,23 @@
+function fibonacciGenerator (n) {
+    
+       var arr1=[];
+    
+       if(n===1)
+       {
+           console.log [0];
+       }
+       else if(n===2)
+       {
+          console.log [0,1];
+       }
+      
+            arr1=[0,1];
+            for(var i=2 ;i<n ;i++)
+            {
+            arr1.push(arr1[arr1.length-2]+arr1[arr1.length-1]);
+            }
+            console.log( arr1);
+            
+    }
+    fibonacciGenerator (4);
+    
